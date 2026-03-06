e09fc0907eaebc36db03c3231af9dc5cee2a09c3
nonsodaboss <nonsodaboss@gmail.com>
2026-03-06 10:18:32 +0100

    added hashrouter

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/React-Router-Checkpoint---MovieApp-with-Navigation/',
   plugins: [
     tailwindcss(),
     react()],
